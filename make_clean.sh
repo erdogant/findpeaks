9c8ab66d61266b7730a7bff09dd9fa3eece7cc37
Erdogan Taskesen <erdogant@gmail.com>
2020-07-04 15:30:18 +0200

    cleaning file update

--- make_clean.sh
+++ make_clean.sh
@@ -5,3 +5,8 @@ rm -rf findpeaks.egg-info
 rm -rf __pycache__
 rm -rf .pytest_cache
 rm -rf .pylint.d
+rm -rf findpeaks/data/*.zip
+rm -rf findpeaks/data/*.csv
+rm -rf *.js
+rm -rf *.html
+rm -rf *.css
