bafdca97407a34d1a961922e26edab4f81cf6079
Erdogan Taskesen <erdogant@gmail.com>
2020-07-31 22:11:11 +0200

    update version

--- findpeaks/__init__.py
+++ findpeaks/__init__.py
@@ -15,7 +15,7 @@ from findpeaks.filters.mean import mean_filter
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.0.2'
+__version__ = '2.0.0'
 
 # module level doc-string
 __doc__ = """
