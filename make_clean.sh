d9c36a4e12cdfcc103fba62c768c83a4925c9f50
Erdogan Taskesen <erdogant@gmail.com>
2020-08-03 14:14:37 +0200

    remove files for cleaning dir

--- make_clean.sh
+++ make_clean.sh
@@ -7,6 +7,10 @@ rm -rf .pytest_cache
 rm -rf .pylint.d
 rm -rf findpeaks/data/*.zip
 rm -rf findpeaks/data/*.csv
+rm -rf findpeaks/data/*.png
+rm -rf findpeaks/__pycache__
+rm -rf findpeaks/filters/__pycache__
+rm -rf findpeaks/utils/__pycache__
 rm -rf *.js
 rm -rf *.html
 rm -rf *.css
