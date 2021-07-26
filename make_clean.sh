9c07aa85adc701aa4427052f2ce84dc79dbcc134
erdogant <erdogant@gmail.com>
2021-07-26 11:30:43 +0200

    update cleaning script

--- make_clean.sh
+++ make_clean.sh
@@ -8,6 +8,7 @@ rm -rf .pylint.d
 rm -rf findpeaks/data/*.zip
 rm -rf findpeaks/data/*.csv
 rm -rf findpeaks/data/*.png
+rm -rf findpeaks/.pylint.d
 rm -rf findpeaks/__pycache__
 rm -rf findpeaks/filters/__pycache__
 rm -rf findpeaks/utils/__pycache__
