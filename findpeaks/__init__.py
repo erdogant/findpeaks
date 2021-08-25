7bbe1c2850173a5c7b4c8ec69e368626963b4764
erdogant <erdogant@gmail.com>
2021-08-25 21:26:42 +0200

    output is changed in this version. The peak and valley is not return when using an image.

--- findpeaks/__init__.py
+++ findpeaks/__init__.py
@@ -15,7 +15,7 @@ from findpeaks.filters.mean import mean_filter
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '2.1.7'
+__version__ = '2.2.0'
 
 # module level doc-string
 __doc__ = """
