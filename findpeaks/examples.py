06f54d3489c0a4c54b3489cee1e65b0b06b6e186
erdogant <erdogant@gmail.com>
2025-08-30 12:55:48 +0200

    updaet examples

--- findpeaks/examples.py
+++ findpeaks/examples.py
@@ -11,6 +11,71 @@ import matplotlib.pyplot as plt
 
 # %%
 
+# %%
+# Import library
+from findpeaks import findpeaks
+import matplotlib.pyplot as plt
+
+# Initialize
+fp = findpeaks(method='mask')
+# fp = findpeaks(scale=True, denoise=None, togray=True, imsize=(300, 300))
+
+# Example 2d image
+X = fp.import_example('2dpeaks')
+# Plot RAW input image
+plt.imshow(X)
+# Fit using mask method
+results = fp.fit(X)
+# Plot the pre-processing steps
+fp.plot_preprocessing()
+
+
+#%%
+
+# Import findpeaks
+from findpeaks import findpeaks
+
+# URL to image
+path = r'https://erdogant.github.io/datasets/images/complex_peaks.png'
+
+# Set findpeaks with its parameters
+fp = findpeaks(method='topology', whitelist='peak', limit=5, denoise='lee_sigma', params={'window': 5})
+
+# Read image from url
+X = fp.imread(path)
+
+# Detect peaks
+results = fp.fit(X)
+
+# Show persistence plot
+fp.plot_persistence()
+# Show plot
+fp.plot(figsize=(25, 14), text=True, marker='x', color='#ff0000', figure_order='vertical', fontsize=10)
+# Mesh plot
+fp.plot_mesh(view=(40, 225), cmap='hot')
+
+# Results in dataframe
+result_df = results['persistence']
+
+"""
+      x    y  birth_level  death_level       score  peak valley
+0   433   95       255.00     0.000000  255.000000  True  False
+1   310   95       255.00     8.920000  246.080000  True  False
+2    62   95       255.00     8.960000  246.040000  True  False
+3   186   95       255.00     9.680000  245.320000  True  False
+4   457  100        85.00    33.600000   51.400000  True  False
+5    39  100        85.00    33.640000   51.360000  True  False
+6   163  100        85.00    34.040000   50.960000  True  False
+7   334  100        85.00    34.160000   50.840000  True  False
+8   210  100        85.00    34.240000   50.760000  True  False
+9   286  100        85.00    34.320000   50.680000  True  False
+...
+...
+46   13   50        15.16     9.833333    5.326667  True  False
+"""
+
+# %%
+
 import matplotlib.pyplot as plt
 # Import library
 from findpeaks import findpeaks
@@ -94,17 +159,46 @@ import numpy as np
 x = np.linspace(0, 100, 200)
 # Create the primary peak (sharp and high)
 peak1 = np.exp(-(x - 30)**2 / (2 * 3**2)) * 50
-# Add a few smaller bumps
+# Add peak 2
 peak2 = np.exp(-(x - 70)**2 / (2 * 5**2)) * 20
+# Add peak 3
 peak3 = np.exp(-(x - 80)**2 / (2 * 3**2)) * 15
 # Combine and add some randomness
 y = peak1 + peak2 + peak3 + np.random.normal(0, 0.5, size=len(x))
 
 from findpeaks import findpeaks
-fp = findpeaks(method="peakdetect", lookahead=100, interpolate=10)
+fp = findpeaks(method="peakdetect", lookahead=100, interpolate=None)
 results = fp.fit(y)
 ax = fp.plot()
 
+fp = findpeaks(method="peakdetect", lookahead=100, interpolate=5)
+results = fp.fit(y)
+ax = fp.plot()
+
+fp = findpeaks(method="peakdetect", lookahead=100, interpolate=8)
+results = fp.fit(y)
+ax = fp.plot()
+
+fp = findpeaks(method="peakdetect", lookahead=100, interpolate=30)
+results = fp.fit(y)
+ax = fp.plot()
+
+
+fp = findpeaks(method="topology")
+results = fp.fit(y)
+plt.plot(results['Xdetect'])
+# results['df'].sort_values(by='rank', key=lambda col: np.where(col==0, np.inf, col), ascending=True, inplace=True)
+results['df']
+
+
+ax = fp.plot(text=True)
+ax = fp.plot_persistence()
+
+fp = findpeaks(method="topology", limit=0.5)
+results = fp.fit(y)
+ax = fp.plot(text=True)
+ax = fp.plot_persistence()
+plt.plot(results['Xdetect'])
 
 # %%
 from findpeaks import findpeaks
@@ -341,7 +435,7 @@ results = fp.fit(img)
 fp.plot_mesh()
 # Create denoised plot
 fp.plot(limit=80, figure_order='horizontal', cmap=plt.cm.hot_r)
-
+plt.plot(results['Xdetect'])
 sum(results['persistence']['score']>80)
 
 # %% Issue 18:
